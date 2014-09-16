882e9db0755dbd2a25a92a06875f292f24478680
vassilux <v.gontcharov@gmail.com>
2014-09-16 18:32:23 +0200

    add test python script

--- tests/import_data_tests.py
+++ tests/import_data_tests.py
@@ -4,10 +4,24 @@ import os
 import sys
 import MySQLdb
 '''
+Please create virtualenv revor 
+mkvirtualenv --no-site-packages revor 
+and install mysql-python with pip
+pip install mysql-python 
+'''
+
+'''
+incomming
 INSERT INTO asteriskcdrdb.cdr (calldate, clid, src, dst, channel, dcontext, disposition,billsec,duration,
 uniqueid,dstchannel, dnid, recordfile, import) VALUES(now(), 'Kebab <551234578>', 
 '551234578', '4002', 'DAHDI/i5/551234578-1', 'incomming', 'ANSWERED', '5', 
 '10', '139412787.59', '', '1157', NULL, 0)
+
+'''
+
+'''
+Outgoing example 
+ '1400147484', '"Cyril Hellstern" <6006>', '6006', '8181', 'SIP/6006-0000001b', 'to-g200be', 'ANSWERED', '0', '1', '1400147484.27', 'SIP/g200beprovider-0000001c', '8181', NULL
 
 '''
 
